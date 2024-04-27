601298dee494a2efd252984217a919968b5713d0
Dams4K <dams4k@users.noreply.github.com>
2024-04-27 11:55:00 +0200

    Falling and jumping animations

--- components/jump.gd
+++ components/jump.gd
@@ -7,8 +7,14 @@ extends PlayerController
 @export var jump: StringName = "ui_accept"
 @export var coyote_time: float = 0.0
 
+## At what velocity we say the player is falling
+@export var falling_y_velocity := 0.0
+
 @onready var coyote_timer: Timer = Timer.new()
 var can_coyote = true
+
+var is_jumping := false
+var is_falling := false
 
 func _ready():
 	coyote_timer.one_shot = true
@@ -22,14 +28,21 @@ func _physics_process(delta: float) -> void:
 	else:
 		can_jump = can_coyote
 	
+	if is_jumping:
+		is_jumping = not player.is_on_floor()
+	
 	if can_jump:
 		if Input.is_action_just_pressed(jump):
+			is_jumping = true
 			can_coyote = false
 			player.velocity = direction * speed
 		
 	if not player.is_on_floor() and can_coyote and coyote_timer.is_stopped() and coyote_time > 0:
 		coyote_timer.wait_time = coyote_time
 		coyote_timer.start()
+	
+	is_falling = player.velocity.y > falling_y_velocity
+	is_jumping = is_jumping and not is_falling
 
 func set_direction(value: Vector2):
 	direction = value
--- player.gd
+++ player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var animation_state: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
 
 @onready var side_movements = $SideMovements
+@onready var jump: Jump = $Jump
 
 func _ready():
 	animation_tree.active = true
@@ -16,9 +17,16 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	var is_moving = side_movements.is_moving
-	animation_tree["parameters/conditions/is_idle"] = !is_moving
-	animation_tree["parameters/conditions/is_moving"] = is_moving
+	var is_jumping = jump.is_jumping
+	var is_falling = jump.is_falling
+	animation_tree["parameters/conditions/is_idle"] = !is_moving and not is_jumping and not is_falling
+	animation_tree["parameters/conditions/is_moving"] = is_moving and not is_jumping and not is_falling
+	animation_tree["parameters/conditions/is_jumping"] = is_jumping
+	animation_tree["parameters/conditions/is_falling"] = is_falling
+	#print(is_jumping, is_falling)
 	
 	if side_movements.is_moving:
 		animation_tree.set("parameters/Idle/blend_position", side_movements.direction)
 		animation_tree.set("parameters/Run/blend_position", side_movements.direction)
+		animation_tree.set("parameters/Jump/blend_position", side_movements.direction)
+		animation_tree.set("parameters/Fall/blend_position", side_movements.direction)
